79fc9c03e96b4c8f82762d51ca318afb1aa3cc96
axiaoxin <254606826@qq.com>
2020-02-15 14:08:10 +0800

    first commit

---
+++ cmd.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/axiaoxin/dateparse"
+)
+
+// GetTimeFromCmd 解析命令行参数中的时间，返回golang的时间对象
+func GetTimeFromCmd() (time.Time, dateparse.DateState) {
+	argsLen := len(os.Args)
+	if argsLen < 2 {
+		log.Fatal("Invalid Args. Please Run as df <time>")
+	}
+	timeArg := strings.Join(os.Args[1:argsLen], " ")
+	// 解析参数为时间对象
+	t, dateState, err := dateparse.ParseAny(timeArg)
+	if err != nil {
+		log.Fatal("Parse time error:", err)
+	}
+	return t, dateState
+}
+
+// GetFormatLayouts 时间戳转字符串时展示的所有格式
+func GetFormatLayouts() (layouts []string) {
+	layouts = []string{
+		"2006-01-02 15:04:05.999999999",
+		"2006/01/02 15:04:05.999999999",
+		time.ANSIC,
+		time.UnixDate,
+		time.RubyDate,
+		time.RFC822,
+		time.RFC822Z,
+		time.RFC850,
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC3339,
+		time.RFC3339Nano,
+		time.Kitchen,
+		time.Stamp,
+		time.StampMilli,
+		time.StampMicro,
+		time.StampNano,
+	}
+	return
+}
+
+func main() {
+	t, dateState := GetTimeFromCmd()
+	if dateState == dateparse.StateTimestamp {
+		for _, layout := range GetFormatLayouts() {
+			fmt.Println(t.Format(layout))
+		}
+	} else {
+		nano := t.UnixNano()
+		fmt.Println(t.Unix())                       // 秒时间戳
+		fmt.Println(nano / int64(time.Millisecond)) // 毫秒时间戳
+		fmt.Println(nano / int64(time.Microsecond)) // 微秒时间戳
+		fmt.Println(nano)                           // 纳秒时间戳
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module df
+
+go 1.13
+
+require github.com/axiaoxin/dateparse v0.0.0-20200214152201-49d0dca1901c
